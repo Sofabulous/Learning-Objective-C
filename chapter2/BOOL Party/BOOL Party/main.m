91af4b8546f4c03d3e20f3914a814f3820faec56
yu kun <jin78700194@gmail.com>
2018-01-17 18:51:13 +0800

    第2章
    Hello Objective-C
    #import
    NSLog()
    BOOL Party
    BOOL
    function
    %@

---
+++ chapter2/BOOL Party/BOOL Party/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BOOL Party
+//
+//  Created by yukun on 2018/1/17.
+//  Copyright © 2018年 SouthWest University-yukun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
---
+++ chapter2/Hello Objective-C/Hello Objective-C/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Hello Objective-C
+//
+//  Created by yukun on 2018/1/16.
+//  Copyright © 2018年 SouthWest University-yukun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    NSLog(@"Hello, Objective-C!");
+    return 0;
+}
