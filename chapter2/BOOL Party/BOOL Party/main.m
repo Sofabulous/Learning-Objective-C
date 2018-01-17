92c6696adf9bf0a6b6b8fed130e033ebb9da3c29
yu kun <jin78700194@gmail.com>
2018-01-17 19:22:22 +0800

    ++

--- chapter2/BOOL Party/BOOL Party/main.m
+++ chapter2/BOOL Party/BOOL Party/main.m
@@ -8,10 +8,36 @@
 
 #import <Foundation/Foundation.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+//returns NO if the two integers have the same
+//value,Yes otherwise
+BOOL areIntsDifferent(int thing1,int thing2)
+{
+    if (thing1 == thing2) {
+        return (NO);
+    } else {
+        return (YES);
     }
+    
+}
+
+//areIntsDifferent
+//given a NO value,return the human-readble
+//string "NO".Otherwise return "YES"
+NSString *boolString(BOOL yesNo)
+{
+    if (yesNo == NO) {
+        return (@"NO");
+    }else {
+        return (@"YES");
+    }
+    
+}//boolString
+
+int main(int argc, const char * argv[]) {
+    BOOL areTheyDifferent;
+    areTheyDifferent = areIntsDifferent(5, 5);
+    NSLog(@"are %d and %d different? %@",5,5,boolString(areTheyDifferent));
+    areTheyDifferent = areIntsDifferent(23, 42);
+    NSLog(@"are %d and %d different? %@",23,42,boolString(areTheyDifferent));
     return 0;
 }
