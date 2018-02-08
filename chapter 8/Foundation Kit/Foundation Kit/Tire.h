dd6334b3035338244dbfb235c91b0bf34dfb39cb
yu kun <jin78700194@gmail.com>
2018-02-09 02:10:40 +0800

    Chapter8 Foundation Kit
     Foundation Kit的有用的、面向数据的简单类和数据类型

---
+++ chapter 8/Foundation Kit/Foundation Kit/Tire.h
@@ -0,0 +1,13 @@
+//
+//  Tire.h
+//  Foundation Kit
+//
+//  Created by yukun on 2018/1/29.
+//  Copyright © 2018年 SouthWest University-yukun. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Tire : Tire
+
+@end
---
+++ chapter 8/Foundation Kit/Foundation Kit/Tire.m
@@ -0,0 +1,13 @@
+//
+//  Tire.m
+//  Foundation Kit
+//
+//  Created by yukun on 2018/1/29.
+//  Copyright © 2018年 SouthWest University-yukun. All rights reserved.
+//
+
+#import "Tire.h"
+
+@implementation Tire
+
+@end
